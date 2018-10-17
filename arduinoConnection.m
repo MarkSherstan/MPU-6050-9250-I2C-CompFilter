b34e80968da05d968ea694cedeac54734f865e04
Mark Sherstan <mshersta@ualberta.ca>
2018-10-17 17:33:18 -0600

    Read accel and gyro reg

--- arduinoConnection.m
+++ arduinoConnection.m
@@ -16,9 +16,35 @@ writeRegister(dev, hex2dec('1B'), hex2dec('08'), 'int16');
 
 
 
-% READ DATA
-ACCEL_XOUT_H = readRegister(dev,hex2dec('3B'),'int8')
-ACCEL_XOUT_L = readRegister(dev,hex2dec('3C'),'int8')
+% Read Accelerometer
+ACCEL_XOUT_H = readRegister(dev,hex2dec('3B'),'int8');
+ACCEL_XOUT_L = readRegister(dev,hex2dec('3C'),'int8');
+
+ACCEL_YOUT_H = readRegister(dev,hex2dec('3D'),'int8');
+ACCEL_YOUT_L = readRegister(dev,hex2dec('3E'),'int8');
+
+ACCEL_ZOUT_H = readRegister(dev,hex2dec('3F'),'int8');
+ACCEL_ZOUT_L = readRegister(dev,hex2dec('40'),'int8');
+
+a.x = typecast(int8([ACCEL_XOUT_H ACCEL_XOUT_L]),'int16');
+a.y = typecast(int8([ACCEL_YOUT_H ACCEL_YOUT_L]),'int16');
+a.z = typecast(int8([ACCEL_ZOUT_H ACCEL_ZOUT_L]),'int16');
+
+% Read Gyroscope
+GYRO_XOUT_H = readRegister(dev,hex2dec('43'),'int8');
+GYRO_XOUT_L = readRegister(dev,hex2dec('44'),'int8');
+
+GYRO_YOUT_H = readRegister(dev,hex2dec('45'),'int8');
+GYRO_YOUT_L = readRegister(dev,hex2dec('46'),'int8');
+
+GYRO_ZOUT_H = readRegister(dev,hex2dec('47'),'int8');
+GYRO_ZOUT_L = readRegister(dev,hex2dec('48'),'int8');
+
+g.x = typecast(int8([GYRO_XOUT_H GYRO_XOUT_L]),'int16');
+g.y = typecast(int8([GYRO_YOUT_H GYRO_YOUT_L]),'int16');
+g.z = typecast(int8([GYRO_ZOUT_H GYRO_ZOUT_L]),'int16');
+
+
 
 % https://arduino.stackexchange.com/questions/36383/what-does-this-notation-stands-for-wire-read-8-wire-read
  % Page 7 --> https://www.invensense.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
