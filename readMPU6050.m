70b46f338185dca1c6909e8f9979c113f100ce6c
MarkSherstan <mshersta@ualberta.ca>
2018-10-21 20:44:00 -0600

    Changed order of low and high bytes

--- readMPU6050.m
+++ readMPU6050.m
@@ -10,9 +10,9 @@ function [a g] = readMPU6050(dev,scaleFactorAccel,scaleFactorGyro)
   ACCEL_ZOUT_H = readRegister(dev,hex2dec('3F'),'int8');
   ACCEL_ZOUT_L = readRegister(dev,hex2dec('40'),'int8');
 
-  a.x = double(typecast(int8([ACCEL_XOUT_H ACCEL_XOUT_L]),'int16')) / scaleFactorAccel;
-  a.y = double(typecast(int8([ACCEL_YOUT_H ACCEL_YOUT_L]),'int16')) / scaleFactorAccel;
-  a.z = double(typecast(int8([ACCEL_ZOUT_H ACCEL_ZOUT_L]),'int16')) / scaleFactorAccel;
+  a.x = double(typecast(int8([ACCEL_XOUT_L ACCEL_XOUT_H]),'int16')) / scaleFactorAccel;
+  a.y = double(typecast(int8([ACCEL_YOUT_L ACCEL_YOUT_H]),'int16')) / scaleFactorAccel;
+  a.z = double(typecast(int8([ACCEL_ZOUT_L ACCEL_ZOUT_H]),'int16')) / scaleFactorAccel;
 
   % Read Gyroscope
   GYRO_XOUT_H = readRegister(dev,hex2dec('43'),'int8');
@@ -24,8 +24,8 @@ function [a g] = readMPU6050(dev,scaleFactorAccel,scaleFactorGyro)
   GYRO_ZOUT_H = readRegister(dev,hex2dec('47'),'int8');
   GYRO_ZOUT_L = readRegister(dev,hex2dec('48'),'int8');
 
-  g.x = double(typecast(int8([GYRO_XOUT_H GYRO_XOUT_L]),'int16')) / scaleFactorGyro;
-  g.y = double(typecast(int8([GYRO_YOUT_H GYRO_YOUT_L]),'int16')) / scaleFactorGyro;
-  g.z = double(typecast(int8([GYRO_ZOUT_H GYRO_ZOUT_L]),'int16')) / scaleFactorGyro;
+  g.x = double(typecast(int8([GYRO_XOUT_L GYRO_XOUT_H]),'int16')) / scaleFactorGyro;
+  g.y = double(typecast(int8([GYRO_YOUT_L GYRO_YOUT_H]),'int16')) / scaleFactorGyro;
+  g.z = double(typecast(int8([GYRO_ZOUT_L GYRO_ZOUT_H]),'int16')) / scaleFactorGyro;
 
 end
