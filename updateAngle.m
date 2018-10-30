f7920f18b950bf3b8aaf9be7723c7fa690595dc6
MarkSherstan <mshersta@ualberta.ca>
2018-10-29 23:08:18 -0600

    Keep angles positive

--- updateAngle.m
+++ updateAngle.m
@@ -1,6 +1,14 @@
 function [angle] = updateAngle(angle)
 
-if (angle <= -360)
+% if (angle <= -360)
+% 	angle = angle + 360;
+% elseif (angle >= 360)
+% 	angle = angle - 360;
+% else
+% 	return
+% end
+
+if (angle < 0)
 	angle = angle + 360;
 elseif (angle >= 360)
 	angle = angle - 360;
