e44626d4e939eba7d114d753c3096b77726abb88
Mark Sherstan <mshersta@ualberta.ca>
2019-01-17 20:54:56 -0700

    Added some notes and made calculation based on loop speed

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -11,11 +11,10 @@ double gyroRoll, gyroPitch, gyroYaw;
 int gyro_x, gyro_y, gyro_z;
 long gyro_x_cal, gyro_y_cal, gyro_z_cal;
 double rotation_x, rotation_y, rotation_z;
-int freq;
+double freq, dt;
 double tau = 0.98;
 double roll = 0;
 double pitch = 0;
-double dt = 0.004;
 
 // 250 deg/s --> 131, 500 deg/s --> 65.5, 1000 deg/s --> 32.8, 2000 deg/s --> 16.4
 long scaleFactorGyro = 65.5;
@@ -52,11 +51,12 @@ void setup() {
   gyro_z_cal /= 3000;
 
   // Display headers
-  Serial.print("\nNote: Yaw is not filtered and will drift!\n\n");
-  Serial.print("Timer\t\t");
-  Serial.print("Roll\t\t");
-  Serial.print("Pitch\t\t");
-  Serial.print("Yaw\t\t\n");
+  Serial.print("\nNote 1: Yaw is not filtered and will drift!\n");
+  Serial.print("\nNote 2: Make sure sampling frequency is ~250 Hz\n");
+  Serial.print("Sampling Frequency (Hz)\t\t");
+  Serial.print("Roll (deg)\t\t");
+  Serial.print("Pitch (deg)\t\t");
+  Serial.print("Yaw (deg)\t\t\n");
   delay(2000);
 
   //Reset the loop timer
@@ -68,6 +68,7 @@ void setup() {
 void loop() {
   freq = 1/((micros() - loopTimer2) * 1e-6);
   loopTimer2 = micros();
+  dt = 1/freq;
 
   // Read the raw acc data from MPU-6050
   read_mpu_6050_data();
@@ -109,13 +110,13 @@ void loop() {
   // Serial.println(accelPitch);
 
   // Data out serial monitor
-  Serial.print(freq);     Serial.print(",");
+  Serial.print(freq,0);   Serial.print(",");
   Serial.print(roll,1);   Serial.print(",");
   Serial.print(pitch,1);  Serial.print(",");
   Serial.println(gyroYaw,1);
 
   // Wait until the loopTimer reaches 4000us (250Hz) before next loop
-  while (micros() - loopTimer < 4000);
+  while (micros() - loopTimer <= 4000);
   loopTimer = micros();
 }
 
