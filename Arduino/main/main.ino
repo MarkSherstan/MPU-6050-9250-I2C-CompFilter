ab949bd552a4135a42c6958b8d34ad37c9c18aeb
Mark Sherstan <mshersta@ualberta.ca>
2019-01-17 20:28:54 -0700

    Tested and verified to work. Played w/ baudrate and Ts for prop integrat

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,38 +1,74 @@
 //Include I2C library and declare variables
 #include <Wire.h>
 
-int gyro_x, gyro_y, gyro_z;
-long loop_timer;
-long scaleFactorGyro = 65.5; // 250 deg/s --> 131, 500 deg/s --> 65.5, 1000 deg/s --> 32.8, 2000 deg/s --> 16.4
-double rotation_x, rotation_y, rotation_z;
-
-long acc_x, acc_y, acc_z;
+long loopTimer, loopTimer2;
 int temperature;
-long scaleFactorAccel = 8192; // 2g --> 16384 , 4g --> 8192 , 8g --> 4096, 16g --> 2048
+double accelPitch;
+double accelRoll;
+long acc_x, acc_y, acc_z;
 double accel_x, accel_y, accel_z;
+double gyroRoll, gyroPitch, gyroYaw;
+int gyro_x, gyro_y, gyro_z;
+long gyro_x_cal, gyro_y_cal, gyro_z_cal;
+double rotation_x, rotation_y, rotation_z;
+int freq;
+double tau = 0.98;
+double roll = 0;
+double pitch = 0;
+double dt = 0.004;
+
+// 250 deg/s --> 131, 500 deg/s --> 65.5, 1000 deg/s --> 32.8, 2000 deg/s --> 16.4
+long scaleFactorGyro = 65.5;
+
+// 2g --> 16384 , 4g --> 8192 , 8g --> 4096, 16g --> 2048
+long scaleFactorAccel = 8192;
 
 
-// Set offsets from initial calibration
-long gyro_x_cal = -213;
-long gyro_y_cal = 240;
-long gyro_z_cal = -297;
-
-
-// Run once
 void setup() {
+  // Start
   Wire.begin();
+  Serial.begin(115200);
 
-  Serial.begin(57600);
-
-  //Setup the registers of the MPU-6050 and start up
+  // Setup the registers of the MPU-6050 and start up
   setup_mpu_6050_registers();
 
+  // Calibration
+  Serial.println("Calibrating gyro, place on level surface and do not move.");
+
+  // // Take 3000 readings for each coordinate and then find average offset
+  for (int cal_int = 0; cal_int < 3000; cal_int ++){
+    if(cal_int % 200 == 0)Serial.print(".");
+    read_mpu_6050_data();
+    gyro_x_cal += gyro_x;
+    gyro_y_cal += gyro_y;
+    gyro_z_cal += gyro_z;
+
+    delay(3);
+  }
+
+  // Average the values
+  gyro_x_cal /= 3000;
+  gyro_y_cal /= 3000;
+  gyro_z_cal /= 3000;
+
+  // Display headers
+  Serial.print("\nNote: Yaw is not filtered and will drift!\n\n");
+  Serial.print("Timer\t\t");
+  Serial.print("Roll\t\t");
+  Serial.print("Pitch\t\t");
+  Serial.print("Yaw\t\t\n");
+  delay(2000);
+
   //Reset the loop timer
-  loop_timer = micros();
+  loopTimer = micros();
+  loopTimer2 = micros();
 }
 
 
 void loop() {
+  freq = 1/((micros() - loopTimer2) * 1e-6);
+  loopTimer2 = micros();
+
   // Read the raw acc data from MPU-6050
   read_mpu_6050_data();
 
@@ -51,19 +87,36 @@ void loop() {
   accel_y = (double)acc_y / (double)scaleFactorAccel;
   accel_z = (double)acc_z / (double)scaleFactorAccel;
 
-  // Print / write data
-  Serial.print(loop_timer); Serial.print(",");
-  Serial.print(accel_x, 7); Serial.print(",");
-  Serial.print(accel_y, 7); Serial.print(",");
-  Serial.print(accel_z, 7); Serial.print(",");
-  Serial.print(rotation_x, 7); Serial.print(",");
-  Serial.print(rotation_y, 7); Serial.print(",");
-  Serial.println(rotation_z, 7);
+  // Complementary filter
+  accelPitch = atan2(accel_y, accel_z) * RAD_TO_DEG;
+  accelRoll = atan2(accel_x, accel_z) * RAD_TO_DEG;
 
+  pitch = (tau)*(pitch + rotation_x*dt) + (1-tau)*(accelPitch);
+  roll = (tau)*(roll - rotation_y*dt) + (1-tau)*(accelRoll);
 
-  // Wait until the loop_timer reaches 4000us (250Hz) before next loop
-  while (micros() - loop_timer < 4000);
-  loop_timer = micros();
+  gyroPitch += rotation_x*dt;
+  gyroRoll -= rotation_y*dt;
+  gyroYaw += rotation_z*dt;
+
+  // Visualize just the roll
+  // Serial.print(roll); Serial.print(",");
+  // Serial.print(gyroRoll); Serial.print(",");
+  // Serial.println(accelRoll);
+
+  // Visualize just the pitch
+  // Serial.print(pitch); Serial.print(",");
+  // Serial.print(gyroPitch); Serial.print(",");
+  // Serial.println(accelPitch);
+
+  // Data out serial monitor
+  Serial.print(freq);     Serial.print(",");
+  Serial.print(roll,1);   Serial.print(",");
+  Serial.print(pitch,1);  Serial.print(",");
+  Serial.println(gyroYaw,1);
+
+  // Wait until the loopTimer reaches 4000us (250Hz) before next loop
+  while (micros() - loopTimer < 4000);
+  loopTimer = micros();
 }
 
 
@@ -93,15 +146,19 @@ void setup_mpu_6050_registers() {
   Wire.write(0x00);
   Wire.endTransmission();
 
-  //Configure the accelerometer
+  // Configure the accelerometer
+  // Wire.write(0x__);
+  // Wire.write; 2g --> 0x00, 4g --> 0x08, 8g --> 0x10, 16g --> 0x18
   Wire.beginTransmission(0x68);
   Wire.write(0x1C);
-  Wire.write(0x08); // 2g --> 0x00, 4g --> 0x08, 8g --> 0x10, 16g --> 0x18
+  Wire.write(0x08);
   Wire.endTransmission();
 
-  //Configure the gyro
+  // Configure the gyro
+  // Wire.write(0x__);
+  // 250 deg/s --> 0x00, 500 deg/s --> 0x08, 1000 deg/s --> 0x10, 2000 deg/s --> 0x18
   Wire.beginTransmission(0x68);
   Wire.write(0x1B);
-  Wire.write(0x08); // 250 deg/s --> 0x00, 500 deg/s --> 0x08, 1000 deg/s --> 0x10, 2000 deg/s --> 0x18
+  Wire.write(0x08);
   Wire.endTransmission();
 }
