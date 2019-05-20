f5815a61fa37f39f24eb3110a0fcf889a6d54c32
Mark Sherstan <mshersta@ualberta.ca>
2019-05-20 12:19:34 -0600

    Update everything into one class and test.

--- RPi/main.py
+++ RPi/main.py
@@ -1,79 +1,178 @@
 import smbus
 import math
+import time
 
+class MPU:
+    def __init__(self, gyro, acc, tau):
+        # Class / object / constructor setup
+        self.gx = None; self.gy = None; self.gz = None;
+        self.ax = None; self.ay = None; self.az = None;
 
+        self.gyroXcal = 0
+        self.gyroYcal = 0
+        self.gyroZcal = 0
 
-# 250 deg/s --> 131, 500 deg/s --> 65.5, 1000 deg/s --> 32.8, 2000 deg/s --> 16.4
-scaleFactorGyro = 65.5
+        self.gyroRoll = 0
+        self.gyroPitch = 0
+        self.gyroYaw = 0
 
-# 2g --> 16384 , 4g --> 8192 , 8g --> 4096, 16g --> 2048
-scaleFactorAccel = 8192.0
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
 
+        self.dtTimer = 0
+        self.tau = tau
 
+        self.gyroScaleFactor, self.gyroHex = self.gyroSensitivity(gyro)
+        self.accScaleFactor, self.accHex = self.accelerometerSensitivity(acc)
 
-################################################################################
-## FUNCTIONS
-################################################################################
+        self.bus = smbus.SMBus(1)
+        self.address = 0x68
 
-# Reads high and low 8 bit values and bit shifts them into 16 bit
-def read_word(reg):
-    h = bus.read_byte_data(address, reg)
-    l = bus.read_byte_data(address, reg+1)
-    value = (h << 8) + l
-    return value
+    def gyroSensitivity(self, x):
+        # Create dictionary with standard value of 500 deg/s
+        return {
+            250:  [131.0, 0x00],
+            500:  [65.5,  0x08],
+            1000: [32.8,  0x10],
+            2000: [16.4,  0x18]
+        }.get(x,  [65.5,  0x08])
 
-# Make 16 bit unsigned value to signed value (0 to 65535) to (-32768 to +32767)
-def read_word_2c(reg):
-    val = read_word(reg)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
+    def accelerometerSensitivity(self, x):
+        # Create dictionary with standard value of 4 g
+        return {
+            2:  [16384.0, 0x00],
+            4:  [8192.0,  0x08],
+            8:  [4096.0,  0x10],
+            16: [2048.0,  0x18]
+        }.get(x,[8192.0,  0x08])
 
+    def setUp(self):
+        # Activate the MPU-6050
+        self.bus.write_byte_data(self.address, 0x6B, 0x00)
 
+        # Configure the accelerometer
+        self.bus.write_byte_data(self.address, 0x1C, self.accHex)
 
-################################################################################
-## Set Up
-################################################################################
-bus = smbus.SMBus(1)
-address = 0x68
+        # Configure the gyro
+        self.bus.write_byte_data(self.address, 0x1B, self.gyroHex)
 
-# Activate the MPU-6050
-bus.write_byte_data(address, 0x6B, 0x00)
+        # Display message to user
+        print("MPU set up:")
+        print('\tAccelerometer: ' + str(self.accHex) + ' ' + str(self.accScaleFactor))
+        print('\tGyro: ' + str(self.gyroHex) + ' ' + str(self.gyroScaleFactor) + "\n")
+        time.sleep(2)
 
-# Configure the accelerometer
-# 2g --> 0x00, 4g --> 0x08, 8g --> 0x10, 16g --> 0x18
-bus.write_byte_data(address, 0x1C, 0x08)
+    def eightBit2sixteenBit(self, reg):
+        # Reads high and low 8 bit values and shifts them into 16 bit
+        h = self.bus.read_byte_data(self.address, reg)
+        l = self.bus.read_byte_data(self.address, reg+1)
+        val = (h << 8) + l
 
-# Configure the gyro
-# 250 deg/s --> 0x00, 500 deg/s --> 0x08, 1000 deg/s --> 0x10, 2000 deg/s --> 0x18
-bus.write_byte_data(address, 0x1B, 0x08)
+        # Make 16 bit unsigned value to signed value (0 to 65535) to (-32768 to +32767)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
 
+    def getRawData(self):
+        self.gx = self.eightBit2sixteenBit(0x43)
+        self.gy = self.eightBit2sixteenBit(0x45)
+        self.gz = self.eightBit2sixteenBit(0x47)
 
+        self.ax = self.eightBit2sixteenBit(0x3B)
+        self.ay = self.eightBit2sixteenBit(0x3D)
+        self.az = self.eightBit2sixteenBit(0x3F)
 
-################################################################################
-## Reading
-################################################################################
+    def calibrateGyro(self, N):
+        # Display message
+        print("Calibrating gyro with " + str(N) + " points. Do not move!")
 
-gyro_x = read_word_2c(0x43) / scaleFactorGyro
-gyro_y = read_word_2c(0x45) / scaleFactorGyro
-gyro_z = read_word_2c(0x47) / scaleFactorGyro
+        # Take N readings for each coordinate and add to itself
+        for ii in range(N):
+            self.getRawData()
+            self.gyroXcal += self.gx
+            self.gyroYcal += self.gy
+            self.gyroZcal += self.gz
 
-acc_x = read_word_2c(0x3b) / scaleFactorAccel
-acc_y = read_word_2c(0x3d) / scaleFactorAccel
-acc_z = read_word_2c(0x3f) / scaleFactorAccel
+        # Find average offset value
+        self.gyroXcal /= N
+        self.gyroYcal /= N
+        self.gyroZcal /= N
 
+        # Display message and restart timer for comp filter
+        print("Calibration complete")
+        print("\tX axis offset: " + str(round(self.gyroXcal,1)))
+        print("\tY axis offset: " + str(round(self.gyroYcal,1)))
+        print("\tZ axis offset: " + str(round(self.gyroZcal,1)) + "\n")
+        time.sleep(2)
+        self.dtTimer = time.time()
 
+    def processIMUvalues(self):
+        # Update the raw data
+        self.getRawData()
 
-################################################################################
-## Display
-################################################################################
-print "Gyroscope: "
-print gyro_x
-print gyro_y
-print gyro_z
+        # Subtract the offset calibration values
+        self.gx -= self.gyroXcal
+        self.gy -= self.gyroYcal
+        self.gz -= self.gyroZcal
 
-print "Accelerometer: "
-print acc_x
-print acc_y
-print acc_z
+        # Convert to instantaneous degrees per second
+        self.gx /= self.gyroScaleFactor
+        self.gy /= self.gyroScaleFactor
+        self.gz /= self.gyroScaleFactor
+
+        # Convert to g force
+        self.ax /= self.accScaleFactor
+        self.ay /= self.accScaleFactor
+        self.az /= self.accScaleFactor
+
+    def compFilter(self):
+        # Get the processed values from IMU
+        self.processIMUvalues()
+
+        # Get delta time and record time for next call
+        dt = time.time() - self.dtTimer
+        self.dtTimer = time.time()
+
+        # Acceleration vector angle
+        accPitch = math.degrees(math.atan2(self.ay, self.az))
+        accRoll = math.degrees(math.atan2(self.ax, self.az))
+
+        # Gyro integration angle
+        self.gyroRoll -= self.gy * dt
+        self.gyroPitch += self.gx * dt
+        self.gyroYaw += self.gz * dt
+        self.yaw = self.gyroYaw
+
+        # Comp filter
+        self.roll = (self.tau)*(self.roll - self.gy*dt) + (1-self.tau)*(accRoll)
+        self.pitch = (self.tau)*(self.pitch + self.gx*dt) + (1-self.tau)*(accPitch)
+
+        # Print data
+        print(" R: " + str(round(self.roll,1)) \
+            + " P: " + str(round(self.pitch,1)) \
+            + " Y: " + str(round(self.yaw,1)))
+
+def main():
+    # Set up class
+    gyro = 250      # 250, 500, 1000, 2000 [deg/s]
+    acc = 2         # 2, 4, 7, 16 [g]
+    tau = 0.98
+    mpu = MPU(gyro, acc, tau)
+
+    # Set up sensor and calibrate gyro with N points
+    mpu.setUp()
+    mpu.calibrateGyro(500)
+
+    # Run for 20 secounds
+    startTime = time.time()
+    while(time.time() < (startTime + 20)):
+        mpu.compFilter()
+
+    # End
+    print("Closing")
+
+# Main loop
+if __name__ == '__main__':
+	main()
