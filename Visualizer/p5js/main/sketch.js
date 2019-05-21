5f1d0e5d633422faacfda4774d735d37d9e27206
Mark Sherstan <mshersta@ualberta.ca>
2019-05-21 15:56:43 -0600

    Begin to play with classes.

--- Visualizer/p5js/main/sketch.js
+++ Visualizer/p5js/main/sketch.js
@@ -1,30 +1,58 @@
+let sensor; // Declare object
+
 function setup() {
   createCanvas(400, 400, WEBGL);
+  sensor = new MPU();
 }
 
 function draw() {
   background(100);
 
-  translate(0, 100, 0);
-  fill('#8000ff');
+  //sensor.displayCube();
+  sensor.displayTorus()
 
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  box(70, 70, 70);
-  pop();
-
-
-  translate(0, -200, 0);
-
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  torus(50, 20);
-  pop();
 }
 
-// https://medium.com/@yyyyyyyuan/tutorial-serial-communication-with-arduino-and-p5-js-cd39b3ac10ce
-// http://p5js.org/examples/
+// Class
+class MPU {
+  constructor() {
+    this.roll = 0;
+    this.pitch = 0;
+    this.yaw = 0;
+    this.count = 1;
+    this.solidFill = fill('#8000ff');
+  }
+
+  upDate() {
+    this.roll = this.count * 0.01;
+    this.pitch = this.count * 0.01;
+    this.yaw = this.count * 0.001;
+    this.count += 1;
+  }
+
+  displayCube() {
+    this.upDate();
+
+    this.solidFill
+
+    push();
+    rotateZ(this.roll);
+    rotateX(this.pitch);
+    rotateY(this.yaw);
+    box(70, 70, 70);
+    pop();
+  }
+
+  displayTorus() {
+    this.upDate();
+
+    this.solidFill
+
+    push();
+    rotateZ(this.roll);
+    rotateX(this.pitch);
+    rotateY(this.yaw);
+    torus(50, 20);
+    pop();
+  }
+}
