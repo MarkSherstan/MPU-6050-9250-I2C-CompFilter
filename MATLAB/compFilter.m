7cccec5d0d9e4c6f625f24c80899ccdba34418c3
Mark Sherstan <mshersta@ualberta.ca>
2019-05-20 23:08:41 -0600

    Set initial values to zero and clean up some function calls.

--- MATLAB/compFilter.m
+++ MATLAB/compFilter.m
@@ -1,12 +1,15 @@
-function [] = compFilter(ard, dev, gyroCal, tau)
+function [] = compFilter(ard, dev, gyroCal, scaleFactorGyro, scaleFactorAcc, tau)
 
-  % Set up and configure MPU for +/- 4g and 500 deg/s
-  scaleFactorAccel = 8192;
-  scaleFactorGyro = 65.5;
-
+  % Set up and configure MPU
   writeRegister(dev, hex2dec('6B'), hex2dec('00'), 'int8'); % Activate mpu
   writeRegister(dev, hex2dec('1C'), hex2dec('08'), 'int8'); % Accelerometer
   writeRegister(dev, hex2dec('1B'), hex2dec('08'), 'int8'); % Gyroscope
+
+  % Initialize values
+  roll = 0;
+  pitch = 0;
+  yaw = 0;
+  previous = 0;
 
   % Start timer and loop until stopped by user (or 30 secounds)
   tic
@@ -14,13 +17,15 @@ function [] = compFilter(ard, dev, gyroCal, tau)
   % Run for 30 seconds
   while toc < 30
     % Read from 6050
-    [a g] = readMPU(dev, scaleFactorAccel, scaleFactorGyro, gyroCal);
+    [a g] = readMPU(dev, scaleFactorAcc, scaleFactorGyro, gyroCal);
 
     % Find angles from accelerometer
     accelPitch = rad2deg(atan2(a.y, a.z));
     accelRoll = rad2deg(atan2(a.x, a.z));
 
+    % Calculate dt
     dt = toc - previous;
+    previous = toc;
 
     % Apply complementary filter
     pitch = (tau)*(pitch + g.x * dt) + (1 - tau)*(accelPitch);
