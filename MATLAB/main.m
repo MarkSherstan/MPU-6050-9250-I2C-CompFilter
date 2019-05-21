72cca2b2d4c800411dddfeea12aa3ddb07a25539
Mark Sherstan <mshersta@ualberta.ca>
2019-05-20 23:08:58 -0600

    Tested and verified main works and that values make sense.

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -1,21 +1,18 @@
-if (exist('a','var') != True
+if (exist('ard','var') ~= 1)
   % Connect to arduino
   ard = arduino;
   dev = i2cdev(ard, '0x68');
 
   % Set up and configure MPU for +/- 4g and 500 deg/s
   scaleFactorGyro = 65.5;
-
-  writeRegister(dev, hex2dec('6B'), hex2dec('00'), 'int8'); % Activate MPU
-  writeRegister(dev, hex2dec('1C'), hex2dec('08'), 'int8'); % Accelerometer
-  writeRegister(dev, hex2dec('1B'), hex2dec('08'), 'int8'); % Gyroscope
+  scaleFactorAcc = 8192;
 
   % Calibrate the gyroscope
-  gyroCal = calibrateGyro(dev,scaleFactorGyro);
+  gyroCal = calibrateGyro(dev, scaleFactorGyro);
 
   % Run the comp filter
-  compFilter(ard, dev, gyroCal, tau)
+  compFilter(ard, dev, gyroCal, scaleFactorGyro, scaleFactorAcc, 0.98)
 else
   % If already started once and variables are saved just run the filter
-  compFilter(ard, dev, gyroCal, tau)
+  compFilter(ard, dev, gyroCal, scaleFactorGyro, scaleFactorAcc, 0.98)
 end
