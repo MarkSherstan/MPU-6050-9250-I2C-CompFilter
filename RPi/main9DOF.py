1411ed1fd2f0475eb1370a60f57703457ca771d7
Mark Sherstan <mshersta@ualberta.ca>
2019-05-28 16:04:31 -0600

    Add mag sensitivity and clean up unused functions

--- RPi/main9DOF.py
+++ RPi/main9DOF.py
@@ -3,7 +3,7 @@ import math
 import time
 
 class MPU:
-    def __init__(self, gyro, acc, tau):
+    def __init__(self, gyro, acc, mag, tau):
         # Class / object / constructor setup
         self.gx = None; self.gy = None; self.gz = None;
         self.ax = None; self.ay = None; self.az = None;
@@ -30,6 +30,7 @@ class MPU:
 
         self.gyroScaleFactor, self.gyroHex = self.gyroSensitivity(gyro)
         self.accScaleFactor, self.accHex = self.accelerometerSensitivity(acc)
+        self.magScaleFactor, self.magHex = self.magnetometerSensitivity(mag)
 
         self.bus = smbus.SMBus(1)
 
@@ -70,6 +71,13 @@ class MPU:
             8:  [4096.0,  0x10],
             16: [2048.0,  0x18]
         }.get(x,[8192.0,  0x08])
+
+    def magnetometerSensitivity(self, x):
+        # Create dictionary with standard value of 16 bit
+        return {
+            14:  [10.0*4912.0/8190.0,  0x06],
+            16:  [10.0*4912.0/32760.0, 0x16],
+        }.get(x,[10.0*4912.0/32760.0,  0x16])
 
     def setUpIMU(self):
         # Check to see if there is a good connection with the MPU 9250
@@ -143,7 +151,7 @@ class MPU:
             # Configure the settings for the mag
             self.bus.write_byte_data(self.MPU9250_ADDRESS, self.I2C_SLV0_ADDR, self.AK8963_ADDRESS);     # Set the I2C slave address of AK8963 and set for write.
             self.bus.write_byte_data(self.MPU9250_ADDRESS, self.I2C_SLV0_REG, self.AK8963_CNTL);         # I2C slave 0 register address from where to begin data transfer
-            self.bus.write_byte_data(self.MPU9250_ADDRESS, self.I2C_SLV0_DO, 0x16);                      # Set magnetometer for 16 bit continous 100 Hz sample rates
+            self.bus.write_byte_data(self.MPU9250_ADDRESS, self.I2C_SLV0_DO, self.magHex);               # Set magnetometer for 14 or 16 bit continous 100 Hz sample rates
             self.bus.write_byte_data(self.MPU9250_ADDRESS, self.I2C_SLV0_CTRL, 0x81);                    # Enable I2C and transfer 1 byte
             time.sleep(0.05)
 
@@ -154,7 +162,7 @@ class MPU:
 
             # Display results to user
             print("MAG set up:")
-            print("\tMagnetometer: " + hex(0x16) + " @ 16 bit 100 Hz" + "\n")
+            print("\tMagnetometer: " + hex(self.magHex) + " " + str(round(self.magScaleFactor,3)) + "\n")
         else:
             # Bad connection or something went wrong
             print("MAG WHO_AM_I was: " + hex(whoAmI) + ". Should have been " + hex(0x48))
@@ -267,63 +275,28 @@ class MPU:
             + " Y: " + str(round(self.yaw,1)))
 
 def main():
-    MPU9250_ADDRESS  = 0x68
-    AK8963_ADDRESS   = 0x0C
+    # Set up class
+    gyro = 250      # 250, 500, 1000, 2000 [deg/s]
+    acc = 2         # 2, 4, 7, 16 [g]
+    mag = 16        # 14, 16 [bit]
+    tau = 0.98
+    mpu = MPU(gyro, acc, mag, tau)
 
-    USER_CTRL        = 0x6A
-    I2C_MST_CTRL     = 0x24
-    I2C_SLV0_ADDR    = 0x25
-    I2C_SLV0_REG     = 0x26
-    I2C_SLV0_CTRL    = 0x27
-    WHO_AM_I_AK8963  = 0x00
-    EXT_SENS_DATA_00 = 0x49
+    # Set up the sensors
+    mpu.setUpIMU()
+    mpu.setUpMAG()
 
-    bus = smbus.SMBus(1)
-
-    print("Who am I IMU? Should be: " + str(0x71))
-    print(bus.read_byte_data(0x68, 0x75)) # Who am I -> should return 0x71
-
-
-    bus.write_byte_data(MPU9250_ADDRESS, USER_CTRL, 0x20);    # Enable I2C Master mode
-    bus.write_byte_data(MPU9250_ADDRESS, I2C_MST_CTRL, 0x0D); # I2C configuration multi-master I2C 400KHz
-
-    bus.write_byte_data(MPU9250_ADDRESS, I2C_SLV0_ADDR, AK8963_ADDRESS | 0x80);    # Set the I2C slave address of AK8963 and set for read.
-    bus.write_byte_data(MPU9250_ADDRESS, I2C_SLV0_REG, WHO_AM_I_AK8963);           # I2C slave 0 register address from where to begin data transfer
-    bus.write_byte_data(MPU9250_ADDRESS, I2C_SLV0_CTRL, 0x81);                     # Enable I2C and transfer 1 byte
-
-    time.sleep(1)
-
-    print("Who am I MAG? Should be: " + str(0x48))
-    print(bus.read_byte_data(MPU9250_ADDRESS, EXT_SENS_DATA_00)) # Who am I -> should return 0x48
-
-    # # Set up class
-    # gyro = 250      # 250, 500, 1000, 2000 [deg/s]
-    # acc = 2         # 2, 4, 7, 16 [g]
-    # tau = 0.98
-    # mpu = MPU(gyro, acc, tau)
-    #
-    # # Set up sensor and calibrate gyro with N points
-    # mpu.setUp()
+    # Calibrate the gyro and mag
     # mpu.calibrateGyro(500)
-    #
+    # mpu.calibrateMag(500)
+
     # # Run for 20 secounds
     # startTime = time.time()
-    # while(time.time() < (startTime + 20)):
+    # while(time.time() < (startTime + 5)):
     #     mpu.compFilter()
-    #
-    # # End
-    # print("Closing")
 
-
-# MFS_16BITS == 0.15 mG per LSB      Mscale = MFS_16BITS
-# M}mode = M_100Hz           Mmode = M_100Hz
-
-# int16_t magCount[3];    // Stores the 16-bit signed magnetometer sensor output
-# float   magCalibration[3] = {0, 0, 0};
-
-# These can be measured once and entered here or can be calculated each time the device is powered on
-# float   magBias[3] = {71.04, 122.43, -36.90}, magScale[3]  = {1.01, 1.03, 0.96}; // Bias corrections for gyro and accelerometer
-
+    # End
+    print("Closing")
 
 # Main loop
 if __name__ == '__main__':
