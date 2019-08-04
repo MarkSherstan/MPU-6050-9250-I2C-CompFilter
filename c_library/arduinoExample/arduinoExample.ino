257e34acc8c6f11225b9f01037da2997be6202dd
Mark Sherstan <mshersta@ualberta.ca>
2019-08-03 19:54:37 -0600

    Update example to reflect 9250 --> XX50

--- c_library/arduinoExample/arduinoExample.ino
+++ c_library/arduinoExample/arduinoExample.ino
@@ -1,6 +1,6 @@
 // Include librarys
 #include <Wire.h>
-#include "MPU9250.h"
+#include "MPUXX50.h"
 
 // Variable definition
 unsigned long loopTimer;
@@ -8,7 +8,7 @@ float dt;
 
 // Setup class
 struct i2c_device_t i2c_dev;
-MPU9250 *mpu9250;
+MPUXX50 *mpuXX50;
 
 // Sensor setup and calibration
 void setup() {
@@ -21,40 +21,40 @@ void setup() {
   i2c_dev.i2c_read = (i2c_read_write_t) &i2c_read;
 
   // Connect to sensor on 0x68
-  mpu9250 = new MPU9250(0x68, i2c_dev);
+  mpuXX50 = new MPUXX50(0x68, i2c_dev);
 
   // Initialize the IMU and set the senstivity values
   Serial.println("---------------------------------------");
-  Serial.print("IMU initialize. Pass/Fail: "); Serial.println(mpu9250->initIMU());
-  Serial.println(mpu9250->getAres(AFS_4G));
-  Serial.println(mpu9250->getGres(GFS_500DPS));
+  Serial.print("IMU initialize. Pass/Fail: "); Serial.println(mpuXX50->initIMU(MPU6050));
+  Serial.println(mpuXX50->getAres(AFS_4G));
+  Serial.println(mpuXX50->getGres(GFS_500DPS));
   delay(1000);
 
   // // Flush out initial data
   // for (int cal_int = 0; cal_int < 5; cal_int ++){
-  //   mpu9250->readRawData();
+  //   mpuXX50->readRawData();
   //   delay(100);
   // }
 
   // Calibrate the gyroscope
   gyro_cal_t gyro_cal;
   Serial.println("Calibrating gyroscope, hold IMU stationary."); delay(2000);
-  mpu9250->gyroCalibration(1000);
+  mpuXX50->gyroCalibration(1000);
 
   // Load saved gyroscope calibration values
   // gyro_cal.x = 0;
   // gyro_cal.y = 0;
   // gyro_cal.z = 0;
-  // mpu9250->setGyroCalibration(gyro_cal);
+  // mpuXX50->setGyroCalibration(gyro_cal);
 
   // Display calibration values to user
-  gyro_cal = mpu9250->getGyroCalibration();
+  gyro_cal = mpuXX50->getGyroCalibration();
 
   Serial.println("---------------------------------------");
   Serial.println("Gyroscope bias values:");
-  Serial.print(mpu9250->gyro_cal.x); Serial.print(",");
-  Serial.print(mpu9250->gyro_cal.y); Serial.print(",");
-  Serial.println(mpu9250->gyro_cal.z);
+  Serial.print(mpuXX50->gyro_cal.x); Serial.print(",");
+  Serial.print(mpuXX50->gyro_cal.y); Serial.print(",");
+  Serial.println(mpuXX50->gyro_cal.z);
   Serial.println("---------------------------------------");
   delay(2000);
 
@@ -70,31 +70,31 @@ void loop() {
   loopTimer = micros();
 
   // Print raw data to the serial monitor
-  // mpu9250->readRawData();
+  // mpuXX50->readRawData();
   //
-  // Serial.print(mpu9250->imu_raw.ax,2); Serial.print(" , ");
-  // Serial.print(mpu9250->imu_raw.ay,2); Serial.print(" , ");
-  // Serial.print(mpu9250->imu_raw.az,2); Serial.print(" , ");
-  // Serial.print(mpu9250->imu_raw.gx,2); Serial.print(" , ");
-  // Serial.print(mpu9250->imu_raw.gy,2); Serial.print(" , ");
-  // Serial.println(mpu9250->imu_raw.gz,2);
+  // Serial.print(mpuXX50->imu_raw.ax,2); Serial.print(" , ");
+  // Serial.print(mpuXX50->imu_raw.ay,2); Serial.print(" , ");
+  // Serial.print(mpuXX50->imu_raw.az,2); Serial.print(" , ");
+  // Serial.print(mpuXX50->imu_raw.gx,2); Serial.print(" , ");
+  // Serial.print(mpuXX50->imu_raw.gy,2); Serial.print(" , ");
+  // Serial.println(mpuXX50->imu_raw.gz,2);
 
   // Print calibrated data to the serial monitor
-  // mpu9250->readCalData();
+  // mpuXX50->readCalData();
   //
-  // Serial.print(mpu9250->imu_cal.ax,2); Serial.print(" , ");
-  // Serial.print(mpu9250->imu_cal.ay,2); Serial.print(" , ");
-  // Serial.print(mpu9250->imu_cal.az,2); Serial.print(" , ");
-  // Serial.print(mpu9250->imu_cal.gx,2); Serial.print(" , ");
-  // Serial.print(mpu9250->imu_cal.gy,2); Serial.print(" , ");
-  // Serial.println(mpu9250->imu_cal.gz,2);
+  // Serial.print(mpuXX50->imu_cal.ax,2); Serial.print(" , ");
+  // Serial.print(mpuXX50->imu_cal.ay,2); Serial.print(" , ");
+  // Serial.print(mpuXX50->imu_cal.az,2); Serial.print(" , ");
+  // Serial.print(mpuXX50->imu_cal.gx,2); Serial.print(" , ");
+  // Serial.print(mpuXX50->imu_cal.gy,2); Serial.print(" , ");
+  // Serial.println(mpuXX50->imu_cal.gz,2);
 
   // Print complementary filter attitude to the serial monitor
-  mpu9250->compFilter(dt, 0.98);
+  mpuXX50->compFilter(dt, 0.98);
 
-  Serial.print(mpu9250->attitude.roll,2); Serial.print(" , ");
-  Serial.print(mpu9250->attitude.pitch,2); Serial.print(" , ");
-  Serial.print(mpu9250->attitude.yaw,2);
+  Serial.print(mpuXX50->attitude.roll,2); Serial.print(" , ");
+  Serial.print(mpuXX50->attitude.pitch,2); Serial.print(" , ");
+  Serial.print(mpuXX50->attitude.yaw,2);
 }
 
 // I2C read and write functions
