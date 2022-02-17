497f462659d001ecbf22e1ec9a5271a831d4383a
Mark Sherstan <mshersta@ualberta.ca>
2022-02-17 00:00:20 -0700

    Add serial port

--- Visualizer/render/main.js
+++ Visualizer/render/main.js
@@ -1,12 +1,29 @@
 // Import modules
+const { SerialPort } = require('serialport')
+const { ByteLengthParser } = require('@serialport/parser-byte-length')
+
 var socket = require('socket.io');
 var express = require('express');
 
-// Configure networking
+
+// Constants
+let serialPortName = '/dev/cu.usbserial-1410';
+let serialBaud = 9600;
+let serverPort = 3000;
+
+// Configure serial port
+const port = new SerialPort({ path: serialPortName, baudRate: serialBaud })
+
+
+// Configure sockets
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(serverPort);
 var io = socket(server);
 app.use(express.static('public'));
+
+// Read data
+const parser = port.pipe(new ByteLengthParser({ length: 14 }))
+parser.on('data', console.log)
 
 // Simulate sending data
 setInterval(sendRotation, 100);
