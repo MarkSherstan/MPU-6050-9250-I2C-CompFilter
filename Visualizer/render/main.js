19eb197999d0c98bf24a4b662dec7f0bf96eb274
Mark Sherstan <mshersta@ualberta.ca>
2022-02-16 23:39:01 -0700

    Clean up

--- Visualizer/render/main.js
+++ Visualizer/render/main.js
@@ -1,23 +1,16 @@
+// Import modules
+var socket = require('socket.io');
 var express = require('express');
+
+// Configure networking
 var app = express();
-
-setInterval(sendRotation, 500);
-
-var rotation = { roll: 1, pitch: 1, yaw: 1 }
-
 var server = app.listen(3000);
+var io = socket(server);
 app.use(express.static('public'));
 
-console.log('Hello world');
-
-var socket = require('socket.io');
-var io = socket(server);
-
-io.sockets.on('connection', newConnection);
-
-function newConnection(socket) {
-    console.log(socket.id);
-}
+// Simulate sending data
+setInterval(sendRotation, 100);
+var rotation = { roll: 1, pitch: 1, yaw: 1 }
 
 function sendRotation() {
     io.sockets.emit('rotation', rotation)
