2063c409703c7953e1e584a8561122bbe8818891
Mark Sherstan <mshersta@ualberta.ca>
2022-02-16 22:59:55 -0700

    Add basic socket functionality

--- Visualizer/render/main.js
+++ Visualizer/render/main.js
@@ -2,5 +2,15 @@ var express = require('express');
 var app = express();
 
 var server = app.listen(3000);
+app.use(express.static('public'));
 
 console.log('Hello world');
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log(socket.id);
+}
--- Visualizer/render/public/sketch.js
+++ Visualizer/render/public/sketch.js
@@ -1,29 +1,6 @@
-////////////////////////////////////////////////////////////////////////////////
-// Variable Declarations
-////////////////////////////////////////////////////////////////////////////////
+var socket;
 
-var serial;
-var ii;
-var ax, ay, az;
-var gx, gy, gz;
-var gyroXcal, gyroYcal, gyroZcal;
-var gyroRoll, gyroPitch, gyroYaw;
-var roll, pitch, yaw;
-var accPitch, accRoll;
-var dtTimer;
-var dt;
-var byteArray = [];
-var calibrationCounter = 0;
-roll = pitch = yaw = 0;
-gyroXcal = gyroYcal = gyroZcal = 0;
-gyroRoll = gyroPitch = gyroYaw = 0;
 
-// Customize these values
-var portName = '/dev/cu.usbmodem14101';
-var tau = 0.98;
-var gyroScaleFactor = 65.5;
-var accScaleFactor = 8192.0;
-var calibrationPts = 100;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Set Up
@@ -33,19 +10,8 @@ function setup() {
     // Create a 3D web canvas to work on
     createCanvas(900, 700, WEBGL);
 
-    // Set up the serial port class
-    serial = new p5.SerialPort();
-
-    // Set some callback functions for the serial port
-    serial.on('open', portOpen);
-    serial.on('data', serialEvent);
-    serial.on('error', serialError);
-
-    // Open a serial port
-    serial.open(portName);
-
-    // Display message for user to hold still
-    print('Calibration to begin. Hold still!\n')
+    // Connect to socket 
+    socket = io.connect('http://localhost:3000')
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -53,151 +19,217 @@ function setup() {
 ////////////////////////////////////////////////////////////////////////////////
 
 function draw() {
+    print('Hello World');
+
     // Draw a fresh background and rotate the frame 90 degrees
     background(150);
     rotateX(PI / 2);
 
     // Display object to the user only if calibration is complete
-    if (calibrationCounter > calibrationPts) {
-        displayObject(roll, pitch, yaw);
-    }
+    // if (calibrationCounter > calibrationPts) {
+    //     displayObject(roll, pitch, yaw);
+    // }
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// Events
-////////////////////////////////////////////////////////////////////////////////
 
-function portOpen() {
-    print('The serial port is opened!');
-}
 
-function serialEvent() {
-    // Reset byteArray
-    byteArray = [];
+// ////////////////////////////////////////////////////////////////////////////////
+// // Variable Declarations
+// ////////////////////////////////////////////////////////////////////////////////
 
-    // Read data from serial port if available and do a header check
-    if ((serial.available() > 14) && (serial.read() == 0x9F) && (serial.read() == 0x6E)) {
-        // Read the useful bytes (Number of signals * Number of bytes for data type)
-        for (ii = 0; ii < (6 * 2); ii++) {
-            byteArray.push(serial.read());
-        }
+// var serial;
+// var ii;
+// var ax, ay, az;
+// var gx, gy, gz;
+// var gyroXcal, gyroYcal, gyroZcal;
+// var gyroRoll, gyroPitch, gyroYaw;
+// var roll, pitch, yaw;
+// var accPitch, accRoll;
+// var dtTimer;
+// var dt;
+// var byteArray = [];
+// var calibrationCounter = 0;
+// roll = pitch = yaw = 0;
+// gyroXcal = gyroYcal = gyroZcal = 0;
+// gyroRoll = gyroPitch = gyroYaw = 0;
 
-        // Cast the bytes into usable values
-        ax = bytes2num(byteArray[1], byteArray[0]);
-        ay = bytes2num(byteArray[3], byteArray[2]);
-        az = bytes2num(byteArray[5], byteArray[4]);
-        gx = bytes2num(byteArray[7], byteArray[6]);
-        gy = bytes2num(byteArray[9], byteArray[8]);
-        gz = bytes2num(byteArray[11], byteArray[10]);
+// // Customize these values
+// var portName = '/dev/cu.usbmodem14101';
+// var tau = 0.98;
+// var gyroScaleFactor = 65.5;
+// var accScaleFactor = 8192.0;
+// var calibrationPts = 100;
 
-        if (calibrationCounter < calibrationPts) {
-            // Sum points until a quota has been met
-            gyroXcal += gx;
-            gyroYcal += gy;
-            gyroZcal += gz;
+// ////////////////////////////////////////////////////////////////////////////////
+// // Set Up
+// ////////////////////////////////////////////////////////////////////////////////
 
-            // Incrament counter
-            calibrationCounter += 1;
+// function setup() {
+//     // Create a 3D web canvas to work on
+//     createCanvas(900, 700, WEBGL);
 
-        } else if (calibrationCounter == calibrationPts) {
-            // Once quota is met find the average offset value
-            gyroXcal /= calibrationPts;
-            gyroYcal /= calibrationPts;
-            gyroZcal /= calibrationPts;
+//     // Set up the serial port class
+//     serial = new p5.SerialPort();
 
-            // Display message
-            print("Calibration complete");
-            print("\tX axis offset: " + String(round(gyroXcal)));
-            print("\tY axis offset: " + String(round(gyroYcal)));
-            print("\tZ axis offset: " + String(round(gyroZcal)) + "\n");
+//     // Set some callback functions for the serial port
+//     serial.on('open', portOpen);
+//     serial.on('data', serialEvent);
+//     serial.on('error', serialError);
 
-            // Start a timer
-            dtTimer = millis();
+//     // Open a serial port
+//     serial.open(portName);
 
-            // Incrament counter once more to show the calibration is complete
-            calibrationCounter += 1;
+//     // Display message for user to hold still
+//     print('Calibration to begin. Hold still!\n')
+// }
 
-        } else {
-            // Turn values into something with a physical representation
-            processValues();
+// ////////////////////////////////////////////////////////////////////////////////
+// // Draw
+// ////////////////////////////////////////////////////////////////////////////////
 
-            // Print values to console
-            print("R: " + round(roll) + " P: " + round(pitch) + " Y: " + round(yaw));
-        }
-    }
-}
+// function draw() {
+//     // Draw a fresh background and rotate the frame 90 degrees
+//     background(150);
+//     rotateX(PI / 2);
 
-function serialError(err) {
-    print('Something went wrong with the serial port. ' + err);
-}
+//     // Display object to the user only if calibration is complete
+//     if (calibrationCounter > calibrationPts) {
+//         displayObject(roll, pitch, yaw);
+//     }
+// }
 
-////////////////////////////////////////////////////////////////////////////////
-// Functions
-////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////
+// // Events
+// ////////////////////////////////////////////////////////////////////////////////
 
-function bytes2num(byteA, byteB) {
-    // Remove byteA sign and & it and then bit shift. Finally combine with byteB
-    var temp = ((byteA & 0x7F) << 8) | byteB;
 
-    // Sign the value
-    if (byteA & 0x80) {
-        temp = temp - 32767;
-    }
+// function serialEvent() {
+//     // Reset byteArray
+//     byteArray = [];
 
-    // Return the number value
-    return temp;
-}
+//     // Read data from serial port if available and do a header check
+//     if ((serial.available() > 14) && (serial.read() == 0x9F) && (serial.read() == 0x6E)) {
+//         // Read the useful bytes (Number of signals * Number of bytes for data type)
+//         for (ii = 0; ii < (6 * 2); ii++) {
+//             byteArray.push(serial.read());
+//         }
 
-function processValues() {
-    // Subract the offset calibration values for the gyro
-    gx -= gyroXcal;
-    gy -= gyroYcal;
-    gz -= gyroZcal;
+//         // Cast the bytes into usable values
+//         ax = bytes2num(byteArray[1], byteArray[0]);
+//         ay = bytes2num(byteArray[3], byteArray[2]);
+//         az = bytes2num(byteArray[5], byteArray[4]);
+//         gx = bytes2num(byteArray[7], byteArray[6]);
+//         gy = bytes2num(byteArray[9], byteArray[8]);
+//         gz = bytes2num(byteArray[11], byteArray[10]);
 
-    // Convert gyro values to instantaneous degrees per second
-    gx /= gyroScaleFactor;
-    gy /= gyroScaleFactor;
-    gz /= gyroScaleFactor;
+//         if (calibrationCounter < calibrationPts) {
+//             // Sum points until a quota has been met
+//             gyroXcal += gx;
+//             gyroYcal += gy;
+//             gyroZcal += gz;
 
-    // Convert accelerometer values to g force
-    ax /= accScaleFactor;
-    ay /= accScaleFactor;
-    az /= accScaleFactor;
+//             // Incrament counter
+//             calibrationCounter += 1;
 
-    // Get delta time and record time for the next call
-    dt = (millis() - dtTimer) * 0.001;
-    dtTimer = millis();
+//         } else if (calibrationCounter == calibrationPts) {
+//             // Once quota is met find the average offset value
+//             gyroXcal /= calibrationPts;
+//             gyroYcal /= calibrationPts;
+//             gyroZcal /= calibrationPts;
 
-    // Acceleration vector angle
-    accPitch = degrees(atan2(ay, az));
-    accRoll = degrees(atan2(ax, az));
+//             // Display message
+//             print("Calibration complete");
+//             print("\tX axis offset: " + String(round(gyroXcal)));
+//             print("\tY axis offset: " + String(round(gyroYcal)));
+//             print("\tZ axis offset: " + String(round(gyroZcal)) + "\n");
 
-    // Gyro integration angle
-    gyroRoll -= gy * dt;
-    gyroPitch += gx * dt;
-    gyroYaw += gz * dt;
+//             // Start a timer
+//             dtTimer = millis();
 
-    // Get attitude of filter using a comp filter and gyroYaw
-    roll = tau * (roll - gy * dt) + (1 - tau) * (accRoll);
-    pitch = tau * (pitch + gx * dt) + (1 - tau) * (accPitch);
-    yaw = gyroYaw;
-}
+//             // Incrament counter once more to show the calibration is complete
+//             calibrationCounter += 1;
 
-function displayObject(roll, pitch, yaw) {
-    // Color options
-    normalMaterial();
+//         } else {
+//             // Turn values into something with a physical representation
+//             processValues();
 
-    // Start display
-    push();
+//             // Print values to console
+//             print("R: " + round(roll) + " P: " + round(pitch) + " Y: " + round(yaw));
+//         }
+//     }
+// }
 
-    // Apply the rotation in Z-Y-X
-    rotateZ(radians(yaw));
-    rotateY(radians(roll));
-    rotateX(radians(pitch));
+// function serialError(err) {
+//     print('Something went wrong with the serial port. ' + err);
+// }
 
-    // Make an object
-    box(200, 200, 200);
+// ////////////////////////////////////////////////////////////////////////////////
+// // Functions
+// ////////////////////////////////////////////////////////////////////////////////
 
-    // Finish display
-    pop();
-}
+// function bytes2num(byteA, byteB) {
+//     // Remove byteA sign and & it and then bit shift. Finally combine with byteB
+//     var temp = ((byteA & 0x7F) << 8) | byteB;
+
+//     // Sign the value
+//     if (byteA & 0x80) {
+//         temp = temp - 32767;
+//     }
+
+//     // Return the number value
+//     return temp;
+// }
+
+// function processValues() {
+//     // Subract the offset calibration values for the gyro
+//     gx -= gyroXcal;
+//     gy -= gyroYcal;
+//     gz -= gyroZcal;
+
+//     // Convert gyro values to instantaneous degrees per second
+//     gx /= gyroScaleFactor;
+//     gy /= gyroScaleFactor;
+//     gz /= gyroScaleFactor;
+
+//     // Convert accelerometer values to g force
+//     ax /= accScaleFactor;
+//     ay /= accScaleFactor;
+//     az /= accScaleFactor;
+
+//     // Get delta time and record time for the next call
+//     dt = (millis() - dtTimer) * 0.001;
+//     dtTimer = millis();
+
+//     // Acceleration vector angle
+//     accPitch = degrees(atan2(ay, az));
+//     accRoll = degrees(atan2(ax, az));
+
+//     // Gyro integration angle
+//     gyroRoll -= gy * dt;
+//     gyroPitch += gx * dt;
+//     gyroYaw += gz * dt;
+
+//     // Get attitude of filter using a comp filter and gyroYaw
+//     roll = tau * (roll - gy * dt) + (1 - tau) * (accRoll);
+//     pitch = tau * (pitch + gx * dt) + (1 - tau) * (accPitch);
+//     yaw = gyroYaw;
+// }
+
+// function displayObject(roll, pitch, yaw) {
+//     // Color options
+//     normalMaterial();
+
+//     // Start display
+//     push();
+
+//     // Apply the rotation in Z-Y-X
+//     rotateZ(radians(yaw));
+//     rotateY(radians(roll));
+//     rotateX(radians(pitch));
+
+//     // Make an object
+//     box(200, 200, 200);
+
+//     // Finish display
+//     pop();
+// }
