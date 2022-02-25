4e69b19f29dd837b50ac8aac2fa04b9272406857
Mark Sherstan <mshersta@ualberta.ca>
2022-02-24 23:08:37 -0700

    Initial commit

---
+++ STM32/Core/Src/MPUXX50.cpp
@@ -0,0 +1,18 @@
+/*
+ * MPUXX50.cpp
+ *
+ *  Created on: Feb 24, 2022
+ *      Author: MarkSherstan
+ */
+
+#include "MPUXX50.h"
+
+MPUXX50::MPUXX50() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MPUXX50::~MPUXX50() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ STM32/Core/Src/MPUXX50.h
@@ -0,0 +1,17 @@
+/*
+ * MPUXX50.h
+ *
+ *  Created on: Feb 24, 2022
+ *      Author: MarkSherstan
+ */
+
+#ifndef SRC_MPUXX50_H_
+#define SRC_MPUXX50_H_
+
+class MPUXX50 {
+public:
+	MPUXX50();
+	virtual ~MPUXX50();
+};
+
+#endif /* SRC_MPUXX50_H_ */
