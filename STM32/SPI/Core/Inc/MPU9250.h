3fdda196af457917e2a58a2211a5811918e32ee5
Mark Sherstan <mshersta@ualberta.ca>
2022-03-14 22:03:39 -0600

    Add read reg and and write

--- STM32/SPI/Core/Inc/MPU9250.h
+++ STM32/SPI/Core/Inc/MPU9250.h
@@ -15,19 +15,21 @@
 #include "SPI.h"
 
 // Defines
-#define CS_SELECT   0
-#define CS_DESELECT 1
+#define CS_SELECT   	0
+#define CS_DESELECT 	1
+#define SPI_TIMOUT_MS	1000
 
 // Structures
 struct MPU_Config
 {
+	// TODO: Set this up as -> notation for practice
 	uint8_t CS_PORT, CS_PIN;
 } mpuConfig;
 
 // Functions
-void MPU_init();
-
 void MPU_CS(uint8_t state);
+void MPU_REG_READ(SPI_HandleTypeDef *SPIx, uint8_t *pReg, uint8_t *pRxData, uint16_t RxSize);
+void MPU_REG_WRITE(SPI_HandleTypeDef *SPIx, uint8_t *pReg);
 
 
 #endif /* INC_MPU9250_H_ */
--- STM32/SPI/Core/Src/MPU9250.c
+++ STM32/SPI/Core/Src/MPU9250.c
@@ -7,10 +7,36 @@
 
 #include "MPU9250.h"
 
+/// @brief Read a specific registry address
+/// @param SPIx Pointer to SPI structure config
+/// @param pReg Pointer containing address and value to write
+void MPU_WRITE(SPI_HandleTypeDef *SPIx, uint8_t *pReg)
+{
+	MPU_CS(CS_SELECT);
+
+	HAL_SPI_Transmit(SPIx, pReg, 2, SPI_TIMOUT_MS);
+
+	MPU_CS(CS_DESELECT);
+}
+
+/// @brief Read a specific registry address
+/// @param SPIx Pointer to SPI structure config
+/// @param pReg Pointer of address to start reading at
+/// @param pRxData Pointer to data buffer
+/// @param RxSize Size of data buffer
+void MPU_REG_READ(SPI_HandleTypeDef *SPIx, uint8_t *pReg, uint8_t *pRxData, uint16_t RxSize)
+{
+	MPU_CS(CS_SELECT);
+
+	HAL_SPI_Transmit(SPIx, pReg, 1, SPI_TIMOUT_MS);
+	HAL_SPI_Receive(SPIx, pRxData, RxSize, SPI_TIMOUT_MS);
+
+	MPU_CS(CS_DESELECT);
+}
+
 /// @brief Set CS state to either start or end transmissions
 /// @param state Set low to select, high to deselect
 void MPU_CS(uint8_t state)
 {
 	HAL_GPIO_WritePin(mpuConfig.CS_PORT, mpuConfig.CS_PIN, state);
 }
-
